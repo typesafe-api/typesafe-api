855df21dd4736adbc6648f48d254312d95971e3f
stuart <stuart@strt-solutions.com>
2022-12-08 19:06:53 +0000

    Revert "test-api: created"
    This reverts commit 2c239e0f6de7b76bb42f1edd7a0704459d047dbe.

--- packages/test-api/src/lib/test-api.spec.ts
+++
@@ -1,7 +0,0 @@
-import { testApi } from './test-api';
-
-describe('testApi', () => {
-  it('should work', () => {
-    expect(testApi()).toEqual('test-api');
-  });
-});
--- packages/test-api/src/lib/test-api.ts
+++
@@ -1,3 +0,0 @@
-export function testApi(): string {
-  return 'test-api';
-}
