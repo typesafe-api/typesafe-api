46286c2079d256fe6593ce817f53ce51559295bd
stuart <stuart@strt-solutions.com>
2022-12-08 18:19:57 +0000

    move compiler test code into lib

--- packages/compiler-test/src/lib/compiler-test.spec.ts
+++
@@ -1,7 +0,0 @@
-import { compilerTest } from './compiler-test';
-
-describe('compilerTest', () => {
-  it('should work', () => {
-    expect(compilerTest()).toEqual('compiler-test');
-  });
-});
--- packages/compiler-test/src/lib/compiler-test.ts
+++
@@ -1,3 +0,0 @@
-export function compilerTest(): string {
-  return 'compiler-test';
-}
