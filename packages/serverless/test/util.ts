08f05a7fb58d9bca877428bb908455ac23ad03fe
stuart <stuart@strt-solutions.com>
2022-12-14 00:05:41 +0000

    packages/serverless/test/util.ts: add support for different request bodies

--- packages/serverless/test/util.ts
+++ packages/serverless/test/util.ts
@@ -1,10 +1,13 @@
 import mockContext from 'aws-lambda-mock-context';
 import createEvent from '@serverless/event-mocks';
 import { APIGatewayProxyEvent } from 'aws-lambda';
+import { AbstractEndpointDef } from '@typesafe-api/core';
 
-export const mockRequest = () => {
+export const mockRequest = <T extends AbstractEndpointDef>(
+  body: T['requestOptions'] = {}
+) => {
   const event = createEvent('aws:apiGateway', {
-    body: '{}',
+    body: JSON.stringify(body),
   } as APIGatewayProxyEvent);
   const context = mockContext();
   return { event, context };
