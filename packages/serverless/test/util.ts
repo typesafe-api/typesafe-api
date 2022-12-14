7efb07c0dac886a0831cee2bca0c29b09799fcf4
stuart <stuart@strt-solutions.com>
2022-12-14 00:17:01 +0000

    packages/serverless/test/util.ts: fix mock request params

--- packages/serverless/test/util.ts
+++ packages/serverless/test/util.ts
@@ -1,14 +1,18 @@
 import mockContext from 'aws-lambda-mock-context';
 import createEvent from '@serverless/event-mocks';
 import { APIGatewayProxyEvent } from 'aws-lambda';
-import { AbstractEndpointDef } from '@typesafe-api/core';
 
-export const mockRequest = <T extends AbstractEndpointDef>(
-  body: T['requestOptions'] = {}
-) => {
+interface MockRequestParams {
+  body?: Record<string, unknown>;
+  pathParameters?: Record<string, unknown>;
+}
+
+export const mockRequest = (params: MockRequestParams) => {
+  const { body = {}, pathParameters = {} } = params;
   const event = createEvent('aws:apiGateway', {
     body: JSON.stringify(body),
-  } as APIGatewayProxyEvent);
+    pathParameters: pathParameters,
+  } as unknown as APIGatewayProxyEvent);
   const context = mockContext();
   return { event, context };
 };
