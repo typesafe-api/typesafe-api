1936fd2ee0060708d9d36d5e9bfca2c9249a601b
stuart <stuart@strt-solutions.com>
2023-01-08 12:00:03 +0000

    test/handler.test.ts: remove handling of square brackets

--- packages/serverless/test/handler.test.ts
+++ packages/serverless/test/handler.test.ts
@@ -1,7 +1,8 @@
-import {relativeToCWD} from '@typesafe-api/serverless';
+import { relativeToCWD } from '@typesafe-api/serverless';
 
-it('relativeToCWD(..) escapes square brackets', async () => {
-  const absPath = process.cwd() + '/my-example/[_id]/testing';
+it('relativeToCWD(..)', async () => {
+  const expectedRelPath = 'my-example/_id/testing';
+  const absPath = `${process.cwd()}/${expectedRelPath}`;
   const relativePath = relativeToCWD(absPath);
-  expect(relativePath).toBe("my-example/\\[_id\\]/testing")
-})
+  expect(relativePath).toBe(expectedRelPath);
+});
