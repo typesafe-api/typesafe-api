0101ee63b694b17afd9ca68f595e5577d98cb954
stuart <stuart@strt-solutions.com>
2023-02-25 13:09:50 +0000

    packages/open-api/src/lib/open-api.ts: del

--- packages/open-api/src/lib/open-api.spec.ts
+++
@@ -1,7 +0,0 @@
-import { openApi } from './open-api';
-
-describe('openApi', () => {
-  it('should work', () => {
-    expect(openApi()).toEqual('open-api');
-  });
-});
--- packages/open-api/src/lib/open-api.ts
+++
@@ -1,3 +0,0 @@
-export function openApi(): string {
-  return 'open-api';
-}
