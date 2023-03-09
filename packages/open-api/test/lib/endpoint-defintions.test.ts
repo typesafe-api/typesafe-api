e55a5e5c43cc0579ea88a76a515fba6253e7d9ea
stuart <stuart@strt-solutions.com>
2023-03-09 14:33:23 +0100

    packages/open-api/test/lib/endpoint-defintions.test.ts: add GetSearchDogsEndpointDef to expected

--- packages/open-api/test/lib/endpoint-defintions.test.ts
+++ packages/open-api/test/lib/endpoint-defintions.test.ts
@@ -14,6 +14,7 @@ it('Generate schema', async () => {
   expect(processedTypes).toEqual([
     'CreateDogEndpointDef',
     'GetDogsEndpointDef',
+    'GetSearchDogsEndpointDef',
     'GetDogEndpointDef',
     'HeaderTestEndpointDef',
     'InternalErrorTestEndpointDef',
