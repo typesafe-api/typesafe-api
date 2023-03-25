2d33719cc24392e00682f857459dd5d096f1ba74
stuart <stuart@strt-solutions.com>
2023-03-25 05:19:09 +0000

    packages/open-api/src/cli.ts: created

---
+++ packages/open-api/src/cli.ts
@@ -0,0 +1,48 @@
+import glob from 'glob-promise';
+import { Command } from 'commander';
+import { generateOpenApi } from './lib/generate-open-api';
+import { endpointDefinitions } from './lib/endpoint-definitions';
+import writeYamlFile from 'write-yaml-file';
+
+const program = new Command();
+
+program
+  .option(
+    '--tsConfigFile <path to tsconfig.json>',
+    'The path to the tsconfig file to use.',
+    'tsconfig.json'
+  )
+  .option('--version <version>', 'The version number to your for your api spec', '1.0.0')
+  .option('--output <filepath>', 'The path to the yaml file where you want to output your API spec', 'openapi.yml')
+  .requiredOption('--title <api title>', 'The title of your api')
+  .requiredOption('--routes <glob path(s)>', 'The glob path or comma separated paths to your routes files, make sure you wrap it in quotes!')
+  .parse(process.argv);
+
+const { version, title, tsConfigFile, routes, output } = program.opts();
+
+const main = async () => {
+  const globPaths = routes.split(',');
+
+  let files: string[] = [];
+  for (const globPath of globPaths) {
+    files = files.concat(await glob(globPath));
+  }
+
+  const endpointDefs = await endpointDefinitions({
+    files,
+    tsConfigFile,
+  });
+
+  const spec = await generateOpenApi({
+    endpointDefs,
+    title,
+    version,
+  });
+
+  await writeYamlFile(output, spec)
+  console.log(`Api spec written to: ${output}`)
+};
+
+main()
+  .then(() => console.log('Finished building api spec.'))
+  .catch(console.error);
