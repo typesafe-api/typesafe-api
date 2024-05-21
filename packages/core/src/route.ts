785c6517e4d895ec656700dc8f86ff36617d700c
stuart <stuart@strt-solutions.com>
2024-05-21 15:56:33 +0100

    packages/core/src/route.ts: adapt type to avoid union

--- packages/core/src/route.ts
+++ packages/core/src/route.ts
@@ -3,10 +3,8 @@ import { AbstractEndpointDef } from './endpoint';
 // Generics are required here even though they are not directly used in this interface.
 // This is used to tie endpoint defs to their respective routes
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export type Route<T extends AbstractEndpointDef> =
-  | {
-      path: string;
-      method: 'get' | 'post' | 'patch' | 'put' | 'delete';
-    }
-  // Setting patch and method to null can be used to deploy a route without exposing it through the API
-  | { path: null; method: null };
+export interface Route<T extends AbstractEndpointDef> {
+  // Setting patch or method to null can be used to deploy a route without exposing it through the API
+  path: string | null;
+  method: 'get' | 'post' | 'patch' | 'put' | 'delete' | null;
+}
