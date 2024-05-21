4f9e2921b8ac1a7b1b2441a5bc01494c5bf1335e
stuart <stuart@strt-solutions.com>
2024-05-21 10:57:10 +0100

    packages/core/src/route.ts: null route

--- packages/core/src/route.ts
+++ packages/core/src/route.ts
@@ -3,7 +3,10 @@ import { AbstractEndpointDef } from './endpoint';
 // Generics are required here even though they are not directly used in this interface.
 // This is used to tie endpoint defs to their respective routes
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export interface Route<T extends AbstractEndpointDef> {
-  path: string;
-  method: 'get' | 'post' | 'patch' | 'put' | 'delete';
-}
+export type Route<T extends AbstractEndpointDef> =
+  | {
+      path: string;
+      method: 'get' | 'post' | 'patch' | 'put' | 'delete';
+    }
+  // Setting patch and method to null can be used to deploy a route without exposing it through the API
+  | { path: null; method: null };
