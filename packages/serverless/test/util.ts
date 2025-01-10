e035cf31b69f2627fcdd3b6916525c26ca511426
stuart <stuart@strt-solutions.com>
2025-01-10 10:56:51 +0000

    packages/serverless/test/util.ts: add queryStringParameters

--- packages/serverless/test/util.ts
+++ packages/serverless/test/util.ts
@@ -5,13 +5,15 @@ import { APIGatewayProxyEvent } from 'aws-lambda';
 interface MockRequestParams {
   body?: Record<string, unknown>;
   pathParameters?: Record<string, unknown>;
+  queryStringParameters?: Record<string, unknown>;
 }
 
 export const mockRequest = (params: MockRequestParams = {}) => {
-  const { body = {}, pathParameters = {} } = params;
+  const { body = {}, pathParameters = {}, queryStringParameters = {} } = params;
   const event = createEvent('aws:apiGateway', {
     body: JSON.stringify(body),
     pathParameters: pathParameters,
+    queryStringParameters,
   } as unknown as APIGatewayProxyEvent);
   const context = mockContext();
   return { event, context };
