9f360c5ba399bcc228f4265c5a108df8e23425ff
stuart <stuart@strt-solutions.com>
2025-03-31 16:03:48 +0100

    packages/core/src/index.ts: del

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,6 @@
 export * from './endpoint';
 export * from './api-client';
 export * from './route';
-export * from './type';
 export * from './error';
 export * from './middleware';
 export * from './utils';
