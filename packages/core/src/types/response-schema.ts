c6f19297dfaa5184d141ce4dc660f3acf64c911a
stuart <stuart@strt-solutions.com>
2025-04-01 18:08:27 +0100

    packages/core/src/types/response-schema.ts: created

---
+++ packages/core/src/types/response-schema.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+import { ZodEmptyObject, ZopOptionalPrimitive } from './zod';
+
+export type RespBody = Record<string, z.ZodTypeAny>;
+export type RespHeaders = Record<string, ZopOptionalPrimitive>;
+
+export type AbstractResponseSchemaShape = {
+  body: z.ZodObject<RespBody> | ZodEmptyObject;
+  headers: z.ZodObject<RespHeaders> | ZodEmptyObject;
+}
+
+export type AbstractResponseSchema = z.ZodObject<AbstractResponseSchemaShape>;
+
+export type AbstractResponse = z.infer<AbstractResponseSchema>;
