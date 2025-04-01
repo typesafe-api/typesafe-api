16c169f6b687396b6b3efde48265868fd7e48c50
stuart <stuart@strt-solutions.com>
2025-04-01 14:53:33 +0100

    packages/core/src/types/zod.ts: add ZodPrimitive and ZopOptionalPrimitive

--- packages/core/src/types/zod.ts
+++ packages/core/src/types/zod.ts
@@ -1,3 +1,5 @@
 import { z } from 'zod';
 
 export type ZodEmptyObject = z.ZodRecord<z.ZodString, z.ZodNever>;
+export type ZodPrimitive = z.ZodString | z.ZodNumber | z.ZodBoolean;
+export type ZopOptionalPrimitive = z.ZodOptional<ZodPrimitive> | ZodPrimitive;
