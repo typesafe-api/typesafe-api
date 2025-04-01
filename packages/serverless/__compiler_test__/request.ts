eac6a51642667abef9a72f2c334014043c64cfb6
stuart <stuart@strt-solutions.com>
2025-04-01 22:12:56 +0100

    packages/serverless/__compiler_test__/request.ts: update epected errors

--- packages/serverless/__compiler_test__/request.ts
+++ packages/serverless/__compiler_test__/request.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 import { CreateDogEndpointDef } from '../../core/test/example-routes';
 import {
   TypesafeApiEvent,
@@ -32,6 +33,7 @@ export const reqTest = async (
     body: {
       _id: 'fake-id',
       name: 'name',
+      // Invalid key
       notAValidKey: 1,
       breed: 'breed',
     },
@@ -39,8 +41,8 @@ export const reqTest = async (
 };
 
 // @expected-compiler-errors-start
-// (16,23): error TS2339: Property 'notValidHeader' does not exist on type '{ myheader: string; }'.
-// (22,20): error TS2339: Property 'invalidBody' does not exist on type 'Dog'.
-// (25,21): error TS2339: Property 'invalidQuery' does not exist on type '{}'.
-// (28,22): error TS2339: Property 'invalidParams' does not exist on type '{}'.
-// (35,7): error TS2353: Object literal may only specify known properties, and 'notAValidKey' does not exist in type 'DogWithId'.
+// (17,23): error TS2339: Property 'notValidHeader' does not exist on type '{ myheader: string; }'.
+// (23,20): error TS2339: Property 'invalidBody' does not exist on type '{ name: string; breed: string; }'.
+// (26,21): error TS2339: Property 'invalidQuery' does not exist on type 'never'.
+// (29,22): error TS2339: Property 'invalidParams' does not exist on type 'never'.
+// (37,7): error TS2353: Object literal may only specify known properties, and 'notAValidKey' does not exist in type '{ name: string; breed: string; _id: string; }'.
