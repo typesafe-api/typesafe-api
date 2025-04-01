3b00f8eaec103cb4eeaa70fc62b64342675a50b7
stuart <stuart@strt-solutions.com>
2025-04-01 22:02:48 +0100

    packages/serverless/src/middleware/typesafe-api.ts: update for zod

--- packages/serverless/src/middleware/typesafe-api.ts
+++ packages/serverless/src/middleware/typesafe-api.ts
@@ -2,38 +2,43 @@ import { MiddlewareObj } from '@middy/core';
 import { APIGatewayProxyEvent } from 'aws-lambda';
 import { AbstractEndpointDef, serialize } from '@typesafe-api/core';
 
-export interface TypesafeApiEvent<
-  T extends AbstractEndpointDef
-> extends APIGatewayProxyEvent {
+export interface TypesafeApiEvent<T extends AbstractEndpointDef>
+  extends APIGatewayProxyEvent {
   typesafeApi: {
-    query: Exclude<T['requestOptions']['query'], undefined>;
-    params: Exclude<T['requestOptions']['params'], undefined>;
-    body: Exclude<T['requestOptions']['body'], undefined>;
-    headers: Exclude<T['requestOptions']['headers'], undefined>;
+    query: T['mergedReq']['query'];
+    params: T['mergedReq']['params'];
+    body: T['mergedReq']['body'];
+    headers: T['mergedReq']['headers'];
   };
 }
 
-export const typesafeApi = <T extends AbstractEndpointDef>(): MiddlewareObj<TypesafeApiEvent<T>> => {
+export const parseEvent = <T extends AbstractEndpointDef>(
+  event: APIGatewayProxyEvent
+): TypesafeApiEvent<T>['typesafeApi'] => {
+  const { body, isBase64Encoded } = event;
+  const decodeBody = isBase64Encoded && body;
+  const data = decodeBody ? Buffer.from(body, 'base64').toString() : body;
+
+  const query = event.queryStringParameters ?? {};
+  const params = event.pathParameters ?? {};
+  const parsedBody = JSON.parse(data ?? '{}');
+  const headers: any = event.headers ?? {};
+
+  return {
+    query: query as T['mergedReq']['query'],
+    params: params as T['mergedReq']['params'],
+    body: parsedBody as T['mergedReq']['body'],
+    headers: headers as T['mergedReq']['headers'],
+  };
+};
+
+export const typesafeApi = <T extends AbstractEndpointDef>(): MiddlewareObj<
+  TypesafeApiEvent<T>
+> => {
   return {
     before: async (request) => {
       const { event } = request;
-      const { body, isBase64Encoded } = event;
-      const decodeBody = isBase64Encoded && body;
-      const data = decodeBody
-        ? Buffer.from(body, 'base64').toString()
-        : body;
-
-      const query = event.queryStringParameters ?? {};
-      const params = event.pathParameters ?? {};
-      const parsedBody = JSON.parse(data ?? '{}');
-      const headers: any = event.headers ?? {};
-      
-      event.typesafeApi = {
-        query: query as T['requestOptions']['query'],
-        params: params as T['requestOptions']['params'],
-        body: parsedBody as T['requestOptions']['body'],
-        headers: headers as T['requestOptions']['headers'],
-      };
+      event.typesafeApi = parseEvent<T>(event);
     },
     after: async (request) => {
       request.response.body = serialize(request.response.body);
