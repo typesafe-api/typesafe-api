3acdb39f368327a8b73379d03485ca633b918329
stuart <stuart@strt-solutions.com>
2025-04-01 18:30:53 +0100

    packages/core/src/endpoint.ts: add schemas

--- packages/core/src/endpoint.ts
+++ packages/core/src/endpoint.ts
@@ -1,21 +1,25 @@
 import { AbstractErrorType } from './error';
-import { AbstractRequest } from './types/request-schema';
+import { AbstractRequest, AbstractRequestSchema, PartialAbstractRequestSchema } from './types/request-schema';
 
 export interface EndpointDef<
   TDefaultReq extends AbstractRequest,
+  TDefaultReqSchema extends PartialAbstractRequestSchema,
   TReq extends Partial<AbstractRequest>,
+  TReqSchema extends AbstractRequestSchema,
   TMergedReq extends AbstractRequest,
   TResp,
   E extends AbstractErrorType
 > {
   defaultReq: TDefaultReq;
+  defaultReqSchema: TDefaultReqSchema;
   req: TReq;
+  reqSchema: TReqSchema;
   mergedReq: TMergedReq;
   resp: TResp;
   errorType: E;
 }
 
-export type AbstractEndpointDef = EndpointDef<any, any, any, any, AbstractErrorType>;
+export type AbstractEndpointDef = EndpointDef<any, any, any, any, any, any, AbstractErrorType>;
 
 export type ResponseBody<T extends AbstractEndpointDef> =
   T['resp']['body'];
