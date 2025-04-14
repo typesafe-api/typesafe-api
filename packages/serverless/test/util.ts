e145e6ec9b4368980c63f8cbfe304a93f2813099
stuart <stuart@strt-solutions.com>
2025-04-14 12:43:42 +0100

    packages/serverless/test/util.ts: set headers

--- packages/serverless/test/util.ts
+++ packages/serverless/test/util.ts
@@ -6,14 +6,16 @@ interface MockRequestParams {
   body?: Record<string, unknown>;
   pathParameters?: Record<string, unknown>;
   queryStringParameters?: Record<string, unknown>;
+  headers?: Record<string, unknown>;
 }
 
 export const mockRequest = (params: MockRequestParams = {}) => {
-  const { body = {}, pathParameters = {}, queryStringParameters = {} } = params;
+  const { body = {}, pathParameters = {}, queryStringParameters = {}, headers = {} } = params;
   const event = createEvent('aws:apiGateway', {
     body: JSON.stringify(body),
     pathParameters: pathParameters,
     queryStringParameters,
+    headers,
   } as unknown as APIGatewayProxyEvent);
   const context = mockContext();
   return { event, context };
